83970b11d68aea699cb0291f663a9f43607e2ca1
apprehensive-pleco <jacob_dobson@outlook.com>
2022-05-19 00:38:47 -0500

    poop

---
+++ git-upload.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Author : Apprehensive-Pleco
+# Copyright (c) Apprehensive-Pleco
+# This Program is to be able to automate the git process such as git add, git commit, and git push.
+
+echo Adding Updates to git
+git add .
+echo Committing Changes to git
+git commit 
+echo Uploading Changes to git
+git push
